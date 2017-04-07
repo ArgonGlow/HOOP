b6030642d0f008756a4eb3208c4e2a0d6448f0ce
ArgonGlow <koen2702@gmail.com>
2017-04-07 19:42:33 +0200

    les 4 base

---
+++ les 4/src/Particle.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ofMain.h"
+
+class Particle {
+
+public:
+	Particle();
+
+	void init(int x, int y);
+
+	void move();
+
+	void draw();
+
+	void setColors(ofColor outer, ofColor inner);
+
+	private:
+        ofPoint location;
+        int radius;
+        ofVec2f speed;
+        long lifetime;
+
+        ofColor innerColor;
+        ofColor outerColor;
+};
---
+++ les 4/src/ParticleEmitter.cpp
@@ -0,0 +1,23 @@
+#include "ParticleEmitter.h"
+
+
+Particle* ParticleEmitter::emit() {
+	Particle* newParticle;
+	newParticle = new Particle();
+	newParticle->setColors(inner, outer);
+	return newParticle;
+}
+
+void ParticleEmitter::setCurvingParticleRatio(float ratio) {
+	curvingParticleRatio = ratio;
+}
+
+void ParticleEmitter::setOrigin(int x, int y) {
+	Xorigin = x;
+	Yorigin = y;
+}
+
+void ParticleEmitter::setColors(ofColor innerColor, ofColor outerColor) {
+	inner = innerColor;
+	outer = outerColor;
+}
